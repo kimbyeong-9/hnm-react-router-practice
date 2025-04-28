53cf02e2e3cfd0158e804095526c8240a49dafca
byeongho Kim <gkrjawnrkf1@gmail.com>
2025-04-28 23:55:39 +0900

    누나 리액트 쇼핑몰앱

---
+++ src/component/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+const Navbar = () => {
+  return (
+  <div>
+  <div>
+    <FontAwesomeIcon icon={faUser}/>
+  </div>
+  <div></div>
+  <div></div>
+</div>
+ );
+};
+
+export default Navbar;
